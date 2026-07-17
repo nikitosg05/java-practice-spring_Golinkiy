0d85c39c8bc6d299d86f4d6672f3335ece92b6a6
nikitosg05 <nikitosg05@gmail.com>
2026-07-17 14:07:46 +0300

    feat: реализована сущность User, регистрация и авторизация в консоли

--- src/main/java/com/praktika/board/BoardApplication.java
+++ src/main/java/com/praktika/board/BoardApplication.java
@@ -7,6 +7,7 @@ import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class BoardApplication {
+
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(BoardApplication.class, args);
         ConsoleUI consoleUI = context.getBean(ConsoleUI.class);
